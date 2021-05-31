b8ede5d1cc714bcab310543dcbb3d1aa359bb8a3
dimaspriyo <dimspriyo@gmail.com>
2021-05-31 14:50:39 +0700

    Add Insert to mongodatabase

--- subscribe-database/main.go
+++ subscribe-database/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"github.com/streadway/amqp"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type Info struct {
+	Name      string `json:"name"`
+	IPv4      string `json:"ipv4"`
+	Byte      int64  `json:"byte"`
+	Timestamp int64  `json:"timestamp"`
+}
 
 func failOnError(err error, msg string) {
 	if err != nil {
@@ -22,7 +30,7 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:root@localhost:27017"))
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
 			panic(err)
@@ -70,16 +78,25 @@ func main() {
 
 	forever := make(chan bool)
 
+	var data Info
 	go func() {
 		for d := range msgs {
-			log.Printf(" [x] %s", d.Body)
-			file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_WRONLY, 0644)
-			if err != nil {
-				fmt.Println(err.Error())
-			}
-			defer file.Close()
 
-			file.Write(d.Body)
+			err := json.Unmarshal(d.Body, &data)
+			if err != nil {
+				panic(err.Error())
+			}
+			_, err = collection.InsertOne(ctx, bson.D{
+				{Key: "name", Value: data.Name},
+				{Key: "ipv4", Value: data.IPv4},
+				{Key: "byte", Value: data.Byte},
+				{Key: "timestamp", Value: data.Timestamp}})
+
+			if err != nil {
+				panic(err.Error())
+			}
+
+			fmt.Printf("Insert Name=%s, IPV4=%s, Byte=%d, Timestamp=%d", data.Name, data.IPv4, data.Byte, data.Timestamp)
 		}
 	}()
 
